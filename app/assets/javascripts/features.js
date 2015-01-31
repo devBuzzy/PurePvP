c7bdf06edfe0457667a2aab770bfcff4365f3af7
Jake <jake0oo0andminecraft@gmail.com>
2015-01-30 20:43:01 -0600

    Navbar activation

--- app/assets/javascripts/features.js
+++ app/assets/javascripts/features.js
@@ -2,6 +2,7 @@ $(function() {
 	$('[data-toggle="tooltip"]').tooltip({'placement': 'top', 'container': 'body'});
 	$("#post-content").markdown({
 		autofocus:false,
-		iconlibrary: "fa"
+		iconlibrary: "fa",
+		fullscreen: { enable: false }
 	});
 });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,20 @@ module ApplicationHelper
     text = "#{time_ago_in_words(post.created_at)} #{t("ago")}"
     link_to text, forem.forum_topic_path(post.topic.forum, post.topic, :anchor => "post-#{post.id}", pagination_param => topic.last_page)
   end
+
+  def active(tab, options = {})
+    active = false
+    options[:controller] ||= params[:controller]
+    options[:forums] ||= false
+    options[:action] ||= nil
+    if options[:forums] && options[:controller].start_with?('forem')
+      active = true
+    elsif 
+      active = tab == options[:controller]
+      if options[:action]
+        active = params[:action] == options[:action]
+      end
+    end
+    return active ? 'active' : ''
+  end
 end
